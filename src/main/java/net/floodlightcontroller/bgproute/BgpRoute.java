9e42a4d11b8b6281be19747833fbd738d620ddf3
Kunihiro Ishiguro <Kunihiro.Ishiguro@access-company.com>
2012-08-14 23:14:37 -0700

    Add net.floodlightcontroller.bgproute.BgpRouteService.

--- src/main/java/net/floodlightcontroller/bgproute/BgpRoute.java
+++ src/main/java/net/floodlightcontroller/bgproute/BgpRoute.java
@@ -2,24 +2,28 @@ package net.floodlightcontroller.bgproute;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
 
-public class BgpRoute implements IFloodlightModule {
+public class BgpRoute implements IFloodlightModule, BgpRouteService {
 
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(BgpRouteService.class);
+		return l;
 	}
 
 	@Override
 	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<Class<? extends IFloodlightService>, IFloodlightService> m = new HashMap<Class<? extends IFloodlightService>, IFloodlightService>();
+		m.put(BgpRouteService.class, this);
+		return m;
 	}
 
 	@Override
@@ -41,4 +45,8 @@ public class BgpRoute implements IFloodlightModule {
 
 	}
 
+	@Override
+	public int getBuffer() {
+		return 0;
+	}
 }
