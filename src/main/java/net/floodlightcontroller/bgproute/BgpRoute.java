b8f923dd775257c5b9da5b8b2b4c42a79b1aaaf8
Kunihiro Ishiguro <Kunihiro.Ishiguro@access-company.com>
2012-08-14 23:45:14 -0700

    register services.

--- src/main/java/net/floodlightcontroller/bgproute/BgpRoute.java
+++ src/main/java/net/floodlightcontroller/bgproute/BgpRoute.java
@@ -9,9 +9,13 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.restserver.IRestApiService;
 
 public class BgpRoute implements IFloodlightModule, BgpRouteService {
 
+	protected IFloodlightProviderService floodlightProvider;
+	
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
 		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
@@ -26,17 +30,21 @@ public class BgpRoute implements IFloodlightModule, BgpRouteService {
 		return m;
 	}
 
+	protected IRestApiService restApi;
+	
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		l.add(BgpRouteService.class);
+		return l;
 	}
 
 	@Override
 	public void init(FloodlightModuleContext context)
 			throws FloodlightModuleException {
-		// TODO Auto-generated method stub
-
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		restApi = context.getServiceImpl(IRestApiService.class);
 	}
 
 	@Override
